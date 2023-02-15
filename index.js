c774e901ad552155ceac8eeb253b94d6b642286c
Rahul K P <rahulkp01aug@gmail.com>
2023-02-15 17:11:23 +0530

    Fixed previous commit

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ app.listen(port,function() {
 
 app.get('/',function(req,res) {
 	if(req.query.lat1) {
-		console.log(lat1+'\n');
+		console.log(req.query.lat1+'\n');
 	}
-	// if(req.query.lon1) {
-	// 	console.log(lon1+'\n');
-	// }
-	// if(req.query.lat2) {
-	// 	console.log(lat2+'\n');
-	// }
-	// if(req.query.lon2) {
-	// 	console.log(lon2+'\n');
-	// }
+	if(req.query.lon1) {
+		console.log(lon1+'\n');
+	}
+	if(req.query.lat2) {
+		console.log(lat2+'\n');
+	}
+	if(req.query.lon2) {
+		console.log(lon2+'\n');
+	}
 	res.send(req.query.lat1);
 });
