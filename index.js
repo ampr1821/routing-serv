a3e995142c22689a87db888165bcc775b466e393
Rahul K P <rahulkp01aug@gmail.com>
2023-02-15 16:44:57 +0530

    added get request

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const port = 5566;
 app.listen(port,function() {
 	console.log('listening on port '+port);
 });
+
+app.get('/',function(req,res) {
+	if(req.query.lat1) {
+		console.log(lat1+'\n');
+	}
+	// if(req.query.lon1) {
+	// 	console.log(lon1+'\n');
+	// }
+	// if(req.query.lat2) {
+	// 	console.log(lat2+'\n');
+	// }
+	// if(req.query.lon2) {
+	// 	console.log(lon2+'\n');
+	// }
+	res.send(req.query.lat1);
+});
