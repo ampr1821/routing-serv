03f655d63037841a2fb5bec68e1d07df1a41b7bc
Rahul K P <rahulkp01aug@gmail.com>
2023-04-01 20:54:14 +0530

    fixed data structure for use with API call

--- get_route.py
+++ get_route.py
@@ -38,22 +38,22 @@ if __name__ == "__main__":
 
     # create the graph
     # 12.903203, 77.648572 12.912441, 77.632885
-    G = ox.graph_from_place("HSR, Bengaluru, India", network_type='drive')
+    G = ox.graph_from_place("HSR ,Bengaluru, India", network_type='drive')
     start_node = ox.distance.nearest_nodes(G, point1[1], point1[0])
     goal_node = ox.distance.nearest_nodes(G, point2[1], point2[0])
     astar = a_star(G)
     path = astar.astar(start_node, goal_node)
     path = list(path)
 
-    # for i in path:
-    #   lon = path[i][1] #lon
-    #   lat = path[i][0] #lat
-    #   print(str(lat) + "," + str(lon))
-    print(path)
+    for i in path:
+        lon = G._node[i]['x'] #lon
+        lat = G._node[i]['y'] #lat
+        print(str(lat) + ","+str(lon))
 
     sys.stdout.flush()
 # command to run (Windows)
 # python get_route.py 12.9246572 77.5582014 13.0110216 77.6747875
-
+#hsr 
+# python get_route.py 12.909229431138986 77.63947874679494 12.914080814688852 77.64155496935096
 # command to run (Linux)
 # python3 get_route.py 12.9246572 77.5582014 13.0110216 77.6747875
